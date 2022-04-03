c97a155b695e92178f8b6b3db84f23bf48a611fb
Nurlytan <nurik.bro.2000@mail.ru>
2022-04-03 17:45:56 +0600

    first commit. Sign up/in with email and sign in with google. Display profile email, image and username. Navigation Drawer released. Fragments working fine.

--- app/src/main/java/com/example/drawermenugoogleauthorization/FragmentProfile.kt
+++ app/src/main/java/com/example/drawermenugoogleauthorization/FragmentProfile.kt
@@ -1,5 +1,6 @@
 package com.example.drawermenugoogleauthorization
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -7,17 +8,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import androidx.core.widget.addTextChangedListener
 import com.example.drawermenugoogleauthorization.databinding.FragmentProfile2Binding
 import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
 import kotlin.math.log10
 
-class FragmentProfile(val act : AppCompatActivity) : Fragment() {
+class FragmentProfile(val act : MainActivity) : Fragment() {
+    private lateinit var tvUsername : TextView
+    private lateinit var tvEmail : TextView
     private lateinit var binding: FragmentProfile2Binding
     private lateinit var accHelper : AccountHelper
     val mAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -29,6 +39,38 @@ class FragmentProfile(val act : AppCompatActivity) : Fragment() {
         binding.btnLogOut.setOnClickListener {
             logOutFromAccount()
         }
+        if(mAuth.currentUser?.photoUrl!= null){
+            Picasso.get().load(mAuth.currentUser!!.photoUrl).into(binding.imageProfile)
+        }else {
+            Picasso.get().load("https://pic.onlinewebfonts.com/svg/img_458488.png")
+                .into(binding.imageProfile)
+        }
+
+        tvUsername = binding.etUsername
+        tvEmail = binding.etEmail
+
+        tvEmail.text = mAuth.currentUser?.email
+        tvUsername.text = mAuth.currentUser?.displayName.toString()
+
+        tvUsername.addTextChangedListener{
+            binding.btnSaveChanges.visibility = View.VISIBLE
+        }
+
+        binding.imageProfile.setOnClickListener {
+            act.pickImageFromGallery()
+        }
+
+        binding.btnSaveChanges.setOnClickListener {
+            Log.d("MyTag", "onCreateView: ${act.isImageChanged}")
+            if(act.isImageChanged){
+                act.saveChangesToFirebase(tvUsername.text.toString(), mAuth.currentUser, act.imageUri)
+            }
+            else{
+                act.saveChangesToFirebase(tvUsername.text.toString(), mAuth.currentUser)
+            }
+
+        }
+
         return binding.root
     }
 
--- app/src/main/java/com/example/drawermenugoogleauthorization/MainActivity.kt
+++ app/src/main/java/com/example/drawermenugoogleauthorization/MainActivity.kt
@@ -1,27 +1,37 @@
 package com.example.drawermenugoogleauthorization
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import com.example.drawermenugoogleauthorization.databinding.ActivityMainBinding
+import com.example.drawermenugoogleauthorization.databinding.FragmentProfile2Binding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    var isImageChanged = false
+    lateinit var imageUri: String
     lateinit var mAuth : FirebaseAuth
     private lateinit var binding : ActivityMainBinding
     private lateinit var tvAccount : TextView
@@ -31,6 +41,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         mAuth = FirebaseAuth.getInstance()
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == Activity.RESULT_OK){
+//                Log.d("MyTag", "onCreate: ${it.data}")
+            }
+        }
+        Log.d("MyTag", "onCreate: ${mAuth.currentUser?.photoUrl}")
         val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.mainToolbar.toolbar, R.string.open, R.string.close)
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
@@ -39,8 +55,33 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             tvAccount = binding.navView.getHeaderView(0).findViewById(R.id.tvEmail)
             tvAccount.text = mAuth.currentUser!!.email.toString()
             ivAccount = binding.navView.getHeaderView(0).findViewById(R.id.imageView)
-            Picasso.get().load(mAuth.currentUser!!.photoUrl).into(ivAccount)
+            if(mAuth.currentUser!!.photoUrl!= null){
+                Picasso.get().load(mAuth.currentUser!!.photoUrl).into(ivAccount)
+            }else{
+                Picasso.get().load("https://pic.onlinewebfonts.com/svg/img_458488.png").into(ivAccount)
+            }
+
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK) {
+            val imageProfile: ImageView = findViewById(R.id.imageProfile)
+            imageProfile.setImageURI(data?.data)
+            isImageChanged = true
+            imageUri = data?.data!!.toString()
+            Log.d("MyTag", "onActivityResult: ${data.data}")
+            Log.d("MyTag", "onActivityResult: ${data.dataString}")
+        }
+    }
+
+    fun pickImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        val saveBtn = findViewById<Button>(R.id.btnSaveChanges)
+        saveBtn.visibility = View.VISIBLE
+        intent.type = "image/*"
+        launcher.launch(intent)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -54,5 +95,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    fun saveChangesToFirebase(username: String, user: FirebaseUser?, profImageUri: String? = mAuth.currentUser?.photoUrl.toString()){
+        val profileUpdates = userProfileChangeRequest {
+            displayName = username
+            photoUri = Uri.parse(profImageUri)
+        }
+        Log.d("MyTag", "saveChangesToFirebase: ${profImageUri}")
+
+        user!!.updateProfile(profileUpdates).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(this, "Changes saved successfully", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Log.d("MyTag", "${it.exception}")
+            }
+        }
     }
 }
