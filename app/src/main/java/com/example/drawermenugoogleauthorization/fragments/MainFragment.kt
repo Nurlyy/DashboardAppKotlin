54aaa9aeba2ad35cc45e41dfae77aedfa2c03b18
Nurlytan <nurik.bro.2000@mail.ru>
2022-04-04 21:51:12 +0600

    Sixth Commit. Show created tasks of user in recyclerview in mainfragment.

--- app/src/main/java/com/example/drawermenugoogleauthorization/fragments/MainFragment.kt
+++ app/src/main/java/com/example/drawermenugoogleauthorization/fragments/MainFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.drawermenugoogleauthorization.MainActivity
 import com.example.drawermenugoogleauthorization.R
 import com.example.drawermenugoogleauthorization.databinding.FragmentMainBinding
+import com.example.drawermenugoogleauthorization.task.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 
 class MainFragment(val act: MainActivity) : Fragment() {
+    private lateinit var mAuth: FirebaseAuth
     private lateinit var binding: FragmentMainBinding
+    private lateinit var dbref: DatabaseReference
+    private lateinit var rcView :RecyclerView
+    private lateinit var taskArrayList: ArrayList<Task>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +33,33 @@ class MainFragment(val act: MainActivity) : Fragment() {
                 CreateUpdateTaskFragment(act)).commit()
             ft.addToBackStack(null)
         }
+        mAuth = FirebaseAuth.getInstance()
+        rcView = binding.rcViewTasks
+        rcView.layoutManager = LinearLayoutManager(act)
+        taskArrayList = arrayListOf<Task>()
+        getUserData()
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    private fun getUserData() {
+        dbref = FirebaseDatabase.getInstance().getReference("Tasks/${mAuth.currentUser?.uid}")
+        dbref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for (taskSnapshot in snapshot.children){
+                        val task = taskSnapshot.getValue(Task::class.java)
+                        taskArrayList.add(task!!)
+                    }
+                    rcView.adapter = FbRcAdapter(taskArrayList)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
 }
--- app/src/main/java/com/example/drawermenugoogleauthorization/task/Task.kt
+++ app/src/main/java/com/example/drawermenugoogleauthorization/task/Task.kt
@@ -6,15 +6,16 @@ import java.util.*
 
 data class Task(
 //    val taskId: String?,
-    val title:String,
+    val title:String? = null,
     val description: String? = null,
-    val year: Int,
-    val month: Int,
-    val dayOfMonth: Int,
-    val hour: Int,
-    val minute: Int,
+    val year: Int? = null,
+    val month: Int? = null,
+    val dayOfMonth: Int? = null,
+    val hour: Int? = null,
+    val minute: Int? = null,
     val notification: Boolean = false,
     val file: String? = null,
     val isDone: Boolean = false,
     val isDeleted: Boolean = false,
+    val isFavourite: Boolean = false,
 )
